aa0aaf3430d7c8eea0af717a30c82f68eed17789
Katie Schmidt <k8codes@gmail.com>
2022-03-28 19:03:16 -0400

    updated User model to reflect user table in database

--- Models/User.cs
+++ Models/User.cs
@@ -2,112 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.IO;
 
 namespace GCRBA.Models
 {
+    public class User
+    {
+        public int UID = 0;
+        public string FirstName = string.Empty;
+        public string LastName = string.Empty;
+        public string Address = string.Empty;
+        public string City = string.Empty;
+        public int intStateID = 0;
+        public string Zip = string.Empty;
+        public string Phone = string.Empty;
+        public string Email = string.Empty;
+        public string Username = string.Empty;
+        public string Password = string.Empty;
+        public int isAdmin = 0;
+        public ActionTypes ActionType = ActionTypes.NoType;
 
-	public class User
-	{
-		public long UID = 0;
-		public string FirstName = string.Empty;
-		public string LastName = string.Empty;
-		// membership type can be set to a certain value if creating account and not a member
-		public string MemberShipType = string.Empty;
-		public string UserName = string.Empty;
-		public string Password = string.Empty;
-		public string Email = string.Empty;
-		public ActionTypes ActionType = ActionTypes.NoType;
+        // obtain current session status
+        public User GetUserSession()
+        {
+            try
+            {
+                // create new instance of User object 
+                User user = new User();
 
-		// Determines if user is logged in - a "read only property procedure"
-		public bool IsAuthenticated
-		{
-			// get is what makes read only - this property is critical to determining if logged in
-			get
-			{
-				// once we log in the uid changes from 0
-				if (UID > 0) return true;
-				return false;
-			}
-		}
+                // check if CurrentUser is null
+                if (HttpContext.Current.Session["CurrentUser"] == null)
+                {
+                    // if it is null, return blank user object 
+                    return user;
+                }
 
-	
-		// a method to return user object  
-		public User Login()
-		{
-			try
-			{
-				Database db = new Database();
-				return db.Login(this);
-			}
-			// error trap
-			catch (Exception ex) { throw new Exception(ex.Message); }
-		}
+                // else, assign CurrentUser info to user object and return User object 
+                user = (User)HttpContext.Current.Session["CurrentUser"];
+                return user;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
 
-		// will return action type
-		public User.ActionTypes Save()
-		{
-			try
-			{
-				Database db = new Database();
-				if (UID == 0)
-				{ //insert new user
-					this.ActionType = db.InsertUser(this);
-				}
-				else
-				{
-					this.ActionType = db.UpdateUser(this);
-				}
-				return this.ActionType;
-			}
-			catch (Exception ex) { throw new Exception(ex.Message); }
-		}
+        // save current session status 
+        public bool SaveUserSession()
+        {
+            try
+            {
+                HttpContext.Current.Session["CurrentUser"] = this;
+                return true;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
 
-		public bool RemoveUserSession()
-		{
-			try
-			{
-				HttpContext.Current.Session["CurrentUser"] = null;
-				return true;
-			}
-			catch (Exception ex) { throw new Exception(ex.Message); }
-		}
+        // remove current session status 
+        public bool RemoveUserSession()
+        {
+            try
+            {
+                HttpContext.Current.Session["CurrentUser"] = null;
+                return true;
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
 
-		public User GetUserSession()
-		{
-			try
-			{
-				User u = new User();
-				if (HttpContext.Current.Session["CurrentUser"] == null)
-				{
-					return u;
-				}
-				u = (User)HttpContext.Current.Session["CurrentUser"];
-				return u;
-			}
-			catch (Exception ex) { throw new Exception(ex.Message); }
-		}
+        // succesful login -  return User object
+        // unsuccessful login - return null 
+        public User Login()
+        {
+            try
+            {
+                Database db = new Database();
+                return db.Login(this);
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
 
-		public bool SaveUserSession()
-		{
-			try
-			{
-				HttpContext.Current.Session["CurrentUser"] = this;
-				return true;
-			}
-			catch (Exception ex) { throw new Exception(ex.Message); }
-		}
-		// enum gives a numerical value an English name so we can understand what it means
-		public enum ActionTypes
-		{
-			NoType = 0,
-			InsertSuccessful = 1,
-			UpdateSuccessful = 2,
-			DuplicateEmail = 3,
-			DuplicateUserID = 4,
-			Unknown = 5,
-			RequiredFieldsMissing = 6,
-			LoginFailed = 7
-		}
-	}
+        public enum ActionTypes
+        {
+            NoType = 0,
+            InsertSuccessful = 1,
+            UpdateSuccessful = 2,
+            DuplicateEmail = 3,
+            DuplicateUserID = 4,
+            Unknown = 5,
+            RequiredFieldMissing = 6,
+            LoginFailed = 7
+        }
+    }
+
+
 }
