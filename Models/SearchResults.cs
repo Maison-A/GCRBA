12ab60808a7ccfa4cb61027c0b8acd78b128d605
Katie Schmidt <k8codes@gmail.com>
2022-04-03 05:43:23 -0400

    after i pulled, it was throwing an error saying that there was no SearchResults class so i added it from the git

--- Models/SearchResults.cs
+++ Models/SearchResults.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace GCRBA.Models {
-	public class SearchResults {
+namespace GCRBA.Models
+{
+	public class SearchResults
+	{
 
 		public List<Models.NewLocation> lstLocations;
 		public int testLocationSearch = 1;
